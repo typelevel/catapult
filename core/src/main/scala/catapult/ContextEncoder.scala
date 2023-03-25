506a524b97c9972ca08d9a1da5b4cd91dad4f7ef
Ben Plommer <ben.plommer@gmail.com>
2023-03-25 12:51:10 +0000

    Add scaladoc for ContextEncoder

--- core/src/main/scala/catapult/ContextEncoder.scala
+++ core/src/main/scala/catapult/ContextEncoder.scala
@@ -2,6 +2,10 @@ package catapult
 
 import com.launchdarkly.sdk.{LDContext, LDUser}
 
+/** A typeclass for converting values of type [[Ctx]] into [[LDContext]]. An instance must be in scope when
+  * evaulating flags against a context represented by the [[Ctx]] type. Instances are provided for [[LDContext]]
+  * and [[LDUser]]; custom instances can be created to allow other types to be used.
+  */
 trait ContextEncoder[Ctx] {
   def encode(ctx: Ctx): LDContext
 }
